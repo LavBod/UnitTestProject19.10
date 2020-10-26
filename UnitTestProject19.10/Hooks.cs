79b4f93333b8a8bc03ff249098af75e1fc9d6237
LavBod <72502425+LavBod@users.noreply.github.com>
2020-10-26 12:22:40 +0200

    Add files via upload

---
+++ UnitTestProject19.10/Hooks.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject19._10
+{
+    class Hooks
+    {
+        protected IWebDriver Driver;
+
+        [SetUp]
+        //[OneTimeSetUp]
+        public void Setup()
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Window.Maximize();
+            //Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            Driver.Navigate().GoToUrl("http://demosite.casqad.org/");
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+    }
+}
